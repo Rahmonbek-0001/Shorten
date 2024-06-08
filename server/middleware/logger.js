377d1c686e95d9ed2e85db798fbf3a1f22207099
Rahmonbek-0001 <madaminovrahmonbek@gmail.com>
2024-06-08 14:45:36 +0500

    server edit

---
+++ server/middleware/logger.js
@@ -0,0 +1,21 @@
+import colors from "colors";
+
+const logger = (req, res, next) => {
+  const methodColors = {
+    GET: "green",
+    POST: "blue",
+    PUT: "yellow",
+    DELETE: "red",
+  };
+
+  const color = methodColors[req.method] || white;
+
+  console.log(
+    `${req.methods} ${req.protocol}://${req.get("host")} ${req.originalUrl}`[
+      color
+    ]
+  );
+  next();
+};
+
+export default logger;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import links from "./router/links.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
+import { log } from "console";
+const port = process.env.PORT || 8000;
+// get directory name
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// setup static folder
+app.use(express.static(path.join(dirname, "public")));
+
+console.log(links);
+
+// Router
+app.use("/api/links", links);
+
+// Error hendler
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
