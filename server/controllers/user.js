0a072784c9c4eecffc1cd5d1b1ceea8314df5b4a
Rahmonbek-0001 <madaminovrahmonbek@gmail.com>
2024-06-19 14:13:21 +0500

    Edit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,9 +8,9 @@ dotenv.config();
 export const signUp = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const user = await Users.find({ 
+    const user = await Users.find({
       name,
-      email, 
+      email,
     });
     if (user?.length !== 0) {
       return res.status(401).json({ message: "User already registered" });
@@ -38,9 +38,28 @@ export const signUp = async (req, res) => {
     console.log(error);
   }
 };
-
-export const login = (req, res) => {
+export const login = async (req, res) => {
   try {
-    const { name, password } = req.body;
-  } catch (error) {}
+    const { email, password } = req.body;
+    const user = await Users.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found please sign up first" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(404).json({ message: "password incorrect" });
+    }
+    const token = jwt.sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.jwt_secret
+    );
+    return res.status(200).json({ message: "login successfully", token, user });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
