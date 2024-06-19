4aa4296f9931160cd785e7567c4e054d67358cf4
Rahmonbek-0001 <madaminovrahmonbek@gmail.com>
2024-06-19 17:28:35 +0500

    Api create

---
+++ client/src/api/auth.js
@@ -0,0 +1,19 @@
+export const userLogin = async (data) => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}api/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    if (response.status !== 200) {
+      const { message } = await response.json()
+      return { message, status: response.status }
+    }
+    const { message, user, token } = await response.json()
+    return { message, user, token }
+  } catch (error) {
+    console.error('Error submitting form:', error)
+  }
+}
