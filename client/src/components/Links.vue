99d5452e8c544ed2c8cbc24abc571530ecd91bb7
Hikmat <xikmatkhiva.4s@gmail.com>
2024-06-27 18:24:55 +0500

    Update Links.vue

--- client/src/components/Links.vue
+++ client/src/components/Links.vue
@@ -2,13 +2,9 @@
 <template>
   <div class="flex justify-center mt-10 relative">
     <h2 v-if="isLoading">sabr qiling</h2>
-  <div class="flex container mx-auto  flex-col justify-center items-center mt-10 relative">
-    <div
-      v-if="isLoading"
-      class="border-2 border-black border-l-white w-14 h-14 rounded-full animate-spin flex justify-center items-center"
-    >
+    <div class="flex container mx-auto flex-col justify-center items-center mt-10 relative">
+      <Link v-for="link in links" :key="link.id" :link="{ ...link }" />
     </div>
-    <Link v-for="link in links" :key="link.id" :link="{ ...link }" />
   </div>
 </template>
 
